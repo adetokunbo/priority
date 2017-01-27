b3497122237d82a8f2f7c9f40c199c9e12c6ad85
Alex Chan <a.chan@wellcome.ac.uk>
2017-01-27 17:41:24 +0000

    Reduce code repetition

--- test/test_priority_stateful.py
+++ test/test_priority_stateful.py
@@ -24,39 +24,28 @@ class PriorityQueue(RuleBasedStateMachine):
             assert stream_id not in self.stream_ids
             self.stream_ids.add(stream_id)
 
-    @rule(stream_id=st.integers())
-    def remove_stream(self, stream_id):
+    def _run_action(self, action, stream_id):
         try:
-            self.tree.remove_stream(stream_id)
+            action(stream_id)
         except priority.MissingStreamError:
             assert stream_id not in self.stream_ids
         except priority.PseudoStreamError:
             assert stream_id == 0
         else:
             assert stream_id in self.stream_ids
-            self.stream_ids.remove(stream_id)
+
+    @rule(stream_id=st.integers())
+    def remove_stream(self, stream_id):
+        self._run_action(self.tree.remove_stream, stream_id)
+        self.stream_ids.discard(stream_id)
 
     @rule(stream_id=st.integers())
     def block_stream(self, stream_id):
-        try:
-            self.tree.block(stream_id)
-        except priority.MissingStreamError:
-            assert stream_id not in self.stream_ids
-        except priority.PseudoStreamError:
-            assert stream_id == 0
-        else:
-            assert stream_id in self.stream_ids
+        self._run_action(self.tree.block, stream_id)
 
     @rule(stream_id=st.integers())
     def unblock_stream(self, stream_id):
-        try:
-            self.tree.unblock(stream_id)
-        except priority.MissingStreamError:
-            assert stream_id not in self.stream_ids
-        except priority.PseudoStreamError:
-            assert stream_id == 0
-        else:
-            assert stream_id in self.stream_ids
+        self._run_action(self.tree.unblock, stream_id)
 
 
 TestPriorityQueueStateful = PriorityQueue.TestCase
