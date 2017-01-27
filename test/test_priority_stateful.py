6d7a5a43da3c36c7ce5f3ec5190e75e7ba457a37
Alex Chan <a.chan@wellcome.ac.uk>
2017-01-27 17:17:08 +0000

    We can remove streams as well

--- test/test_priority_stateful.py
+++ test/test_priority_stateful.py
@@ -1,3 +1,6 @@
+import random
+
+from hypothesis import note
 from hypothesis import strategies as st
 from hypothesis.stateful import RuleBasedStateMachine, rule
 
@@ -20,6 +23,17 @@ class PriorityQueue(RuleBasedStateMachine):
         else:
             assert stream_id not in self.stream_ids
             self.stream_ids.append(stream_id)
+
+    @rule(seed=st.integers())
+    def remove_stream(self, seed):
+        random.seed(seed)
+        stream_id = random.choice(self.stream_ids)
+        note('Removing stream_id = %s' % stream_id)
+        try:
+            self.tree.remove_stream(stream_id)
+            self.stream_ids.remove(stream_id)
+        except priority.PseudoStreamError:
+            assert stream_id == 0
 
 
 TestPriorityQueueStateful = PriorityQueue.TestCase
