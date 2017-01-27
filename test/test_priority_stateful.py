3446a9b83ba3a9e516107ea6d40225e91bb919ba
Alex Chan <a.chan@wellcome.ac.uk>
2017-01-27 17:37:09 +0000

    Use a set, not a list

--- test/test_priority_stateful.py
+++ test/test_priority_stateful.py
@@ -12,7 +12,7 @@ class PriorityQueue(RuleBasedStateMachine):
     def __init__(self):
         super(PriorityQueue, self).__init__()
         self.tree = priority.PriorityTree()
-        self.stream_ids = [0]
+        self.stream_ids = set([0])
 
     @rule(stream_id=st.integers())
     def insert_stream(self, stream_id):
@@ -22,7 +22,7 @@ class PriorityQueue(RuleBasedStateMachine):
             assert stream_id in self.stream_ids
         else:
             assert stream_id not in self.stream_ids
-            self.stream_ids.append(stream_id)
+            self.stream_ids.add(stream_id)
 
     @rule(stream_id=st.integers())
     def remove_stream(self, stream_id):
